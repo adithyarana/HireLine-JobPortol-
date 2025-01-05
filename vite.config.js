e05405ece882c62047afccaef9af8a6e9b5ae431
adithya <adithyarana447@gmail.com>
2025-01-05 11:05:04 +0530

    viteee

--- vite.config.js
+++ vite.config.js
@@ -38,53 +38,22 @@
 // });
 
 /* eslint-disable no-undef */
-// import path from "path";
-// import react from "@vitejs/plugin-react";
-// import { defineConfig } from "vite";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//     chunkSizeWarningLimit: 2000,
-//   },
-// });
-
-
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) {
-              return "react"; // Separate React and React DOM
-            }
-            if (id.includes("@reduxjs") || id.includes("redux")) {
-              return "redux"; // Separate Redux-related libraries
-            }
-            if (id.includes("lodash")) {
-              return "lodash"; // Separate Lodash
-            }
-            return id.toString().split("node_modules/")[1].split("/")[0]; // Split by individual library
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 2000, // Increased limit for warnings
+    chunkSizeWarningLimit: 2000,
   },
 });
+
+
+
+
 
